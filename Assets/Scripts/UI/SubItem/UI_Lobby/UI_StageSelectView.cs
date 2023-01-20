8971225812ff86c8a5d05aa887457aa91281f376
Hoo <adobephotoshop@naver.com>
2023-01-20 15:38:48 +0900

    UI바인딩작업중

---
+++ Assets/Scripts/UI/SubItem/UI_Lobby/UI_StageSelectView.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_StageSelectView : UI_Base
+{
+    public enum Texts
+    {
+        StageNameText,
+        StageSubText,
+    }
+    public enum Images
+    {
+        StageNameOutline,
+        BossImage,
+        BossImage2,
+    }
+    public enum GameObjects
+    {
+        UI_StageCarousel,
+        PreBtn,
+        NextBtn,
+        UI_MonsterList
+    }
+    public override void Init()
+    {
+
+    }
+}
